c9fb6a770e28b920b91c1cae245c5275d3d55eaa
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-12-13 19:11:40 -0700

    commit tree faa677880f9956053514ca93518a5863abbcef21

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,15 @@
 def admin_login(username, password)
-  # your code here
+  if ((username == "ADMIN" || "admin") && password == "12345") then "Access granted"
+  elsif ((username != "ADMIN" || "admin")) then "Access denied"
+  else "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40 then "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65 then "It's a little chilly out there!"
+  elsif temperature > 85 then "It's too dang hot out there!"
+  else "It's perfect out there!"
 end
 
 def fizzbuzz(num)
